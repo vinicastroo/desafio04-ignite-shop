f4624cdccdfd1ed35c741ab0976fba47283fa528
Vinicius Castro da Costa <viniciusc.d.c@hotmail.com>
2022-10-13 19:23:44 -0300

    mudança min height

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -5,7 +5,7 @@ export const HomeContainer = styled('main', {
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
-  minHeight: 500,
+  minHeight: 600,
   padding: '0 1rem'
 })
 
